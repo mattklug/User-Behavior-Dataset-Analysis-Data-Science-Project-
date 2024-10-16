28ae6c4f69c4f2bc7315bfc32912142908446ebb
Matthew <mattklug17@gmail.com>
2024-10-16 18:06:24 -0500

    Testing making a change and committing through GitHub

--- analysis.py
+++ analysis.py
@@ -1,4 +1,16 @@
 # This is where we will write our program
 # Data Science Projecthow do
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('user_behavior_dataset_csv')
+
+print(df.head())
+print(df.info())
+print(df.describe())
+
+
+
 print("What's up team")
